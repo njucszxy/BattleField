a1b6eb709d3ec89d699f72c5072bda83c3ab93dc
周新宇 <njuzxy@zhouxinyudeMacBook-Pro.local>
2018-09-26 18:40:48 +0800

    Committer: 周新宇 <njuzxy@zhouxinyudeMacBook-Pro.local>  Changes to be committed: 	modified:   BattleField.java

--- BattleField.java
+++ BattleField.java
@@ -32,12 +32,34 @@ public class BattleField {
                 land[i][j] = false;
     }
 
-    //CB Formation
-    void RandomSort(int[] queue, int queueLength)
+    //General Formation
+    void ZigFormation(int[][] positionMap, CAMP camp)
     {
-
+        //Formation
+        int column = 0;
+        if(camp == CAMP.CB)
+            column = 2;
+        else if(camp == CAMP.MO)
+            column = 7;
+        else
+            System.out.println("Error CAMP!");
+        positionMap[4][column] = 1;
+        positionMap[3][column] = 2;
+        positionMap[5][column] = 3;
+        positionMap[2][column] = 4;
+        positionMap[6][column] = 5;
+        positionMap[1][column] = 6;
+        positionMap[7][column] = 7;
+    }
+    void FishFormation(int[][] positionMap,CAMP camp)
+    {
+        //Formation
+    }
+    void SnakeFormation(int[][] positionMap, CAMP camp)
+    {
         //Get random queue
-        //int[] queue = new int[7];
+        int queueLength = 7;
+        int []queue = new int[queueLength];
         for(int j = 0;j < queueLength;j++)
             queue[j] = -1;
         int ran = 0;
@@ -60,10 +82,23 @@ public class BattleField {
                 count++;
             }
         }
+        //Formation
+        if(camp == CAMP.CB)
+        {
+            for(int i = 0;i < 7;i++)
+                positionMap[1+i][2] = queue[i];
+        }
+        else if(camp == CAMP.MO)
+        {
+            for(int i = 0;i < 7;i++)
+                positionMap[1+i][7] = queue[i];
+        }
+        else
+            System.out.println("Error CAMP!");
     }
 
-    
 
+    //CB Formation
     public void CBFormation(FormationType ft)
     {
         switch(ft)
